0cc465a03022f6f2b757485182d4339bb192fc81
akhnay <85334180+akhnay@users.noreply.github.com>
2022-09-10 14:01:37 +0530

    directives created

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,11 +7,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe('Test', "Some Test", "https://dummyimage.com/600x400/000/fff")
+    new Recipe('Test', "Some Test", "https://dummyimage.com/600x400/000/fff"),
+    new Recipe('Test2', "Some Test2", "https://dummyimage.com/600x400/000/fff"),
+    new Recipe('Test3', "Some Test3", "https://dummyimage.com/600x400/000/fff"),
   ];
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe)
+  }
 }
