c00fd1a98552b0794d0ea38676c9e39e3aa53a56
akhnay <85334180+akhnay@users.noreply.github.com>
2022-09-19 17:46:22 +0530

    subject

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,38 +2,46 @@ import { EventEmitter, Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class RecipeService {
-recipeSelected = new EventEmitter<Recipe>();
+  recipeSelected = new Subject<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe('Test', 'Some Test', 'https://dummyimage.com/600x400/000/fff', [
-        new Ingredient('Apples', 5),
-        new Ingredient('Tomato', 10),
-      ]),
-    new Recipe('Test2', 'Some Test2', 'https://dummyimage.com/600x400/000/fff', [
+      new Ingredient('Apples', 5),
+      new Ingredient('Tomato', 10),
+    ]),
+    new Recipe(
+      'Test2',
+      'Some Test2',
+      'https://dummyimage.com/600x400/000/fff',
+      [
         new Ingredient('Apples', 5),
         new Ingredient('Tomato', 10),
         new Ingredient('Cucumber', 10),
-      ]),
-    new Recipe('Test3', 'Some Test3', 'https://dummyimage.com/600x400/000/fff', [
-        new Ingredient('Apples', 5),
-        new Ingredient('Tomato', 10),
-      ]),
+      ]
+    ),
+    new Recipe(
+      'Test3',
+      'Some Test3',
+      'https://dummyimage.com/600x400/000/fff',
+      [new Ingredient('Apples', 5), new Ingredient('Tomato', 10)]
+    ),
   ];
 
-  constructor(private slService:ShoppingListService){}
+  constructor(private slService: ShoppingListService) {}
 
-  getRecipes(){
+  getRecipes() {
     return this.recipes.slice();
   }
 
-  getRecipe(index: number){
+  getRecipe(index: number) {
     return this.recipes[index];
   }
 
-  addIngredientsToShoppingList(ingredients :Ingredient[]){
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
   }
 }
